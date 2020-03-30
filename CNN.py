e6f52c36cbce5558366f76f6345a3ef752aeae67
pengyuzhang-97 <pengyu.zhang@ufl.edu>
2020-03-29 20:52:25 -0400

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -1,1 +1,41 @@
 #
+import pickle
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+import torch.nn as nn
+from sklearn.model_selection import train_test_split, cross_val_score
+
+
+train_loader = DataLoader(dataset=train_loader, batch_size=572, shuffle=True)
+
+num_epochs = 25
+num_classes = 8
+learning_rate = 0.0005
+
+# Model
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.drop_out = nn.Dropout()
+        self.fc1 = nn.Linear(12 * 12 * 128, 1000)
+        self.fc2 = nn.Linear(1000, 8)
+
+# Forward
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.drop_out(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
