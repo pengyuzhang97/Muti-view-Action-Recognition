c85e40c8bcf10b66dbf5f88e494a9f653779dd4a
pengyuzhang-97 <pengyu.zhang@ufl.edu>
2020-04-27 21:02:21 -0400

    update

--- Ultimate test.py
+++ Ultimate test.py
@@ -12,7 +12,7 @@ class args:
         self.batch_size = 50
         self.num_epochs = 1
         self.lr = 0.00005
-        self.epoch = 1
+        self.epoch = 90
 
 arg = args()
 
@@ -67,7 +67,7 @@ class ConvNet(nn.Module):
             nn.Conv2d(50, 50, kernel_size=2, stride=1, padding=0),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
-        self.drop_out = nn.Dropout()
+        self.drop_out = nn.Dropout(0.5)
         self.fc1 = nn.Linear(6*4*50, 1000)
         self.fc2 = nn.Linear(1000, 500)
     def forward(self, x):
@@ -86,16 +86,15 @@ class CNN_LSTM(nn.Module):
         self.cnn = ConvNet()
         self.lstm = nn.LSTM(input_size=500,
                            hidden_size=64,
-                           num_layers=1, # h and c both have one layer
+                           num_layers=1,
                            batch_first=False)
-        self.linear = nn.Linear(64,11) # h->out requiring passing a fully-connected layer to match # of labels which is 11 different action
+        self.linear = nn.Linear(64,11)
     def forward(self,x):
         c_out = self.cnn(x)
         c_out = torch.unsqueeze(c_out,1)
-        h0 = torch.randn(1,1,64) # initialize h0  #############################################################################
-        c0 = torch.randn(1,1,64) # initialize c0  #############################################################################
+        h0 = torch.randn(1,1,64) # initialize h0
+        c0 = torch.randn(1,1,64) # initialize c0
         r_out, (h, c) = self.lstm(c_out,(h0,c0))
-        #r_out = self.linear(r_out)
         h = self.linear(h)
         return r_out, h, c, c_out
 
@@ -104,23 +103,16 @@ model2 = CNN_LSTM()# camera 2
 model3 = CNN_LSTM()# camera 3
 model4 = CNN_LSTM()# camera 4
 model5 = CNN_LSTM()# camera 5
-# requires 5 datasets, and each of them contains data from one camera
 
 
-#data1 = torch.randn(120*arg.batch_size, 64, 48)
-data = np.load('data from cam1.npy')
+data = np.load('data from cam5.npy') #############################################################################################
 data_ = torch.FloatTensor(data)
 # Then I need to add one dimension which represents number of channels
 data1 = torch.unsqueeze(data_, 1)
 
 # create labels
-label_in = np.load('labels from cam1.npy')
+label_in = np.load('labels from cam5.npy')##########################################################################################
 labels = torch.tensor(label_in)
-
-'''data_test = []
-for i in range(len(resized_data)):
-	train_dataset.append((resized_data[i],labels[i]))'''
-
 
 data_set = []
 for  i in range(int(len(data1)/50)):
@@ -128,12 +120,13 @@ for  i in range(int(len(data1)/50)):
 
 dataloader1 = DataLoader(dataset=data_set, batch_size=1, shuffle=True)
 
+
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model1.parameters(), lr=arg.lr)
+optimizer = torch.optim.Adam(model5.parameters(), lr=arg.lr)##################################################################################
 
 # train model
-loss_list = np.zeros(len(labels))
+loss_list = []
 output = torch.tensor([])
 correct = np.zeros(arg.epoch)
 acc_list = []
@@ -145,10 +138,10 @@ for epoch in range(arg.epoch):
     for i, (images, labels_) in enumerate(dataloader1):
         images = torch.squeeze(images,0)
         '''Forward'''
-        r_out, h, _, c_out = model1(images) # the last vector of r_out will be my feature vector
+        r_out, h, _, c_out = model5(images) ########################################################################################################
         output = torch.squeeze(h,1)
         loss = criterion(output, labels_.long())
-        loss_list[i] = loss
+        loss_list.append(loss.item())
 
         '''Backpropgation'''
         optimizer.zero_grad()
@@ -168,23 +161,21 @@ for epoch in range(arg.epoch):
             print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
                   .format(epoch + 1, arg.epoch, i + 1, len(dataloader1), loss.item(),
                           (correct[epoch] / total[epoch]) * 100))
-        if i == len(dataloader1)-1:
-            print('Feature vector: {}%'.format(r_out[-1]))
+        '''if i == len(dataloader1)-1:
+            print('Feature vector: {}%'.format(r_out[-1]))'''
 
 
+##########################################################################################################
+torch.save(model5.cnn.layer1.state_dict(), '5net_params_layer1.pkl')
+torch.save(model5.cnn.layer2.state_dict(), '5net_params_layer2.pkl')
+torch.save(model5.cnn.layer3.state_dict(), '5net_params_layer3.pkl')
+torch.save(model5.cnn.fc1.state_dict(), '5net_params_linear1.pkl')
+torch.save(model5.cnn.fc2.state_dict(), '5net_params_linear2.pkl')
 
-torch.save(model1.cnn.layer1.state_dict(), '1net_params_layer1.pkl')
-torch.save(model1.cnn.layer2.state_dict(), '1net_params_layer2.pkl')
-torch.save(model1.cnn.layer3.state_dict(), '1net_params_layer3.pkl')
-torch.save(model1.cnn.fc1.state_dict(), '1net_params_linear1.pkl')
-torch.save(model1.cnn.fc2.state_dict(), '1net_params_linear2.pkl')
+torch.save(model5.lstm.state_dict(), '5net_params_lstm.pkl')
+torch.save(model5.linear.state_dict(), '5net_params_linear.pkl')
 
-torch.save(model1.lstm.state_dict(), '1net_params_lstm.pkl')
-torch.save(model1.linear.state_dict(), '1net_params_linear2.pkl')
-
-
-# loss plot
-# h0 c0 initializaiton (LSTM)
-# optimizer(SGD or Adam or whatever effective)
-# CUDA command
-#
+np.save('loss_list 5',loss_list)
+np.save('correct 5', correct)
+np.save('feature vector 5',vector.detach().numpy())
+##############################################################################################################
